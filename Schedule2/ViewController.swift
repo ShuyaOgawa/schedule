4f669aae8939ad4ab5367b588048c57444c22807
小川秀哉 <shuya464924@gmail.com>
2018-11-26 16:16:52 +0900

    Initial Commit

---
+++ Schedule2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schedule2
+//
+//  Created by 小川秀哉 on 2018/11/26.
+//  Copyright © 2018年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
